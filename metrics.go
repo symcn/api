d5730c43c7776d6b2af636c44dce979504e74fc2
champly <champly@outlook.com>
2022-04-24 14:45:02 +0800

    update Metrics interface
    Signed-off-by: champly <champly@outlook.com>

--- metrics.go
+++ metrics.go
@@ -6,44 +6,41 @@ import (
 
 // Metrics is a wrapper interface for prometheus
 type Metrics interface {
-	// Counter creates or returns a prometheus counter by key
-	// if the key is registered by other interface, it will be panic
-	Counter(key string) prometheus.Counter
+	// Counter creates or returns a prometheus counter by name
+	// if the key is registered by other interface, it will return nil
+	// Counter: invoke CounterWithLabelsWithError(name, nil) and ignore error
+	// CounterWithLabels: invoke CounterWithLabelsWithError(name, dynamicLables) and ignore error
+	Counter(name string) prometheus.Counter
+	CounterWithLabelsWithError(name string, dynamicLabels map[string]string) (prometheus.Counter, error)
+	CounterWithLabels(name string, dynamicLabels map[string]string) prometheus.Counter
 
 	// Gauge creates or returns a prometheus gauge by key
-	// if the key is registered by other interface, it will be panic
+	// if the key is registered by other interface, it will return nil
+	// Gauge: invoke GaugeWithLabelsWithError(name, nil) and ignore error
+	// GaugeWithLabels: invoke GaugeWithLabelsWithError(name, dynamicLables) and ignore error
 	Gauge(key string) prometheus.Gauge
+	GaugeWithLabels(name string, dynamicLabels map[string]string) prometheus.Gauge
+	GaugeWithLabelsWithError(name string, dynamicLabels map[string]string) (prometheus.Gauge, error)
 
 	// Histogram creates or returns a prometheus histogram by key
-	// if the key is registered by other interface, it will be panic
+	// if the key is registered by other interface, it will be return nil
+	// Histogram: invoke HistogramWithLabelsWithError(name, buckets, nil) and ignore error
+	// HistogramWithLabels: invoke HistogramWithLabelsWithError(name, buckets, dynamicLables) and ignore error
 	Histogram(key string, buckets []float64) prometheus.Histogram
+	HistogramWithLabels(name string, buckets []float64, dynamicLabels map[string]string) prometheus.Histogram
+	HistogramWithLabelsWithError(name string, buckets []float64, dynamicLabels map[string]string) (prometheus.Histogram, error)
 
 	// Summary creates or returns a summary histogram by key, objectives default 50%, 90% 95% and 99%
-	// if the key is registered by other interface, it will be panic
+	// if the key is registered by other interface, it will be return nil
+	// Summary: invoke SummaryWithLabelsWithError(name, objectives, nil) and ignore error
+	// SummaryWithLables: invoke SummaryWithLabelsWithError(name, objectives, dynamicLables) and ignore error
 	Summary(key string, objectives map[float64]float64) prometheus.Summary
+	SummaryWithLables(name string, objectives map[float64]float64, dynamicLabels map[string]string) prometheus.Summary
+	SummaryWithLabelsWithError(name string, objectives map[float64]float64, dynamicLabels map[string]string) (prometheus.Summary, error)
 
 	// UnregisterAll unregister all metrics.  (Mostly for testing.)
 	UnregisterAll()
-}
 
-// MetricsWithLabels is a wrapper interface for prometheus.Metrics.
-// Usually create only one metcis object, because the name will registry once.
-type MetricsWithLabels interface {
-	// Counter creates or returns a prometheus counter by labels
-	Counter(labels map[string]string) (prometheus.Counter, error)
-
-	// Gauge creates or returns a prometheus gauge by labels
-	Gauge(labels map[string]string) (prometheus.Gauge, error)
-
-	// Histogram creates or returns a prometheus histogram by labels
-	Histogram(labels map[string]string, buckets []float64) (prometheus.Histogram, error)
-
-	// Summary creates or returns a summary histogram by labels, objectives default 50%, 90% 95% and 99%
-	Summary(labels map[string]string, objectives map[float64]float64) (prometheus.Summary, error)
-
-	// Delete prometheus.Metrics With Labels
-	Delete(labels map[string]string) bool
-
-	// Free nnregister from Prometheus
-	Free()
+	// Delete With metricsVec, not unregister metrics
+	DeleteWithLabels(name string, labels map[string]string) bool
 }
